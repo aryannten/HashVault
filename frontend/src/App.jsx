a4f0fa0f87d47b9bc9045e2631b1d0296a99512a
unknown <atharv.avhad13@gmail.com>
2026-02-20 09:05:24 +0530

    Complete project structure for backend and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+function App() {
+  return (
+    <div className="container">
+      <nav><h1>HashVault</h1></nav>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
